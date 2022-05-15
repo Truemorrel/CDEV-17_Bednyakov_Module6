941a235a18ca2b8b37e11f55a948de72eb2ab91b
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-16 01:23:06 +0300

    master: add task 6.6.1 and 6.6.2

---
+++ Task_6_6_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task_6_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrafficLight tl = new TrafficLight();
+            tl.GetLight();
+            tl.ChangeColor();
+            tl.GetLight();
+            tl.ChangeColor();
+            tl.GetLight();
+            tl.ChangeColor();
+            tl.GetLight();
+            tl.ChangeColor();
+            tl.GetLight();
+        }
+        class TrafficLight
+        {
+            private Color _lightColor;
+            public TrafficLight()
+            {
+                _lightColor = Color.red;
+            }
+            public Color GetLight()
+            {
+                return _lightColor;
+            }
+            public void ChangeColor()
+            {
+                _lightColor++;
+            }
+        }
+        enum Color:byte
+        {
+            red = 1,
+            yellow,
+            green
+        }
+    }
+}
---
+++ Task_6_6_2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Task_6_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+		class User
+		{
+			private int age;
+			private string login;
+			private string email;
+
+			public int Age
+			{
+				get
+				{
+					return age;
+				}
+
+				set
+				{
+					if (value < 18)
+					{
+						Console.WriteLine("Возраст должен быть не меньше 18");
+					}
+					else
+					{
+						age = value;
+					}
+				}
+			}
+			public string Login
+            {
+				get
+                {
+					return login;
+                }
+				set
+                {
+					if (value.Length <= 3)
+                    {
+                        Console.WriteLine("login mast have more than 3 symbols");
+                    }
+					else
+                    {
+						login = value;
+                    }
+                }
+            }
+			private string Email
+			{
+				get
+				{
+					return email;
+				}
+				set
+                {
+					if (value.Contains("@"))
+					{
+						login = value;
+					}
+				}
+			}
+		}
+	}
+}
